17ffdc22357fd218ff3de340ad6c01e9de6f788a
Shankara21 <lazuardit21@gmail.com>
2023-03-11 18:06:22 +0700

    Initialization Technical Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { AppsComponent } from './pages/apps/apps.component';
+import { ArticleComponent } from './pages/article/article.component';
+import { MainComponent } from './pages/main/main.component';
+import { TeamComponent } from './pages/team/team.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: MainComponent },
+  { path: 'article', component: ArticleComponent },
+  { path: 'apps', component: AppsComponent },
+  {path:'team', component:TeamComponent},
+  { path: '**', redirectTo: '/dashboard' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { MainComponent } from './pages/main/main.component';
+import { ArticleComponent } from './pages/article/article.component';
+import { AppsComponent } from './pages/apps/apps.component';
+import { TeamComponent } from './pages/team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    MainComponent,
+    ArticleComponent,
+    AppsComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
